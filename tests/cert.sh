d16909b522acd7e779a65bd3ba13c5cb5533e2f1
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2019-03-25 14:11:30 +0100

    Self-signed certificate as described by the manual
    With all the correct key usage options, well, mostly

--- tests/cert.sh
+++ tests/cert.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+if [[ ! -f noise.bin ]]; then
+	 openssl rand -out noise.bin 4096
+fi
 if [[ -f test_cert.pem ]] && [[ -f test_key.pem ]]; then
 	echo "Reusing previous certificate"
 else
-	openssl req -x509 -newkey rsa:2048 -sha256 -keyout test_key.pem -out test_cert.pem -days 2 -nodes -subj "/C=CA/ST=Ontario/L=Toronto/O=Test Company/OU=Testing Department/CN=ldaptest.local"
+	openssl req -x509 -newkey rsa:2048 -sha256 -keyout test_key.pem -out test_cert.pem -days 365 -nodes -extensions eku -config openssl.cnf -subj "/CN=ldaptest.local"
 fi
 # https://stackoverflow.com/a/4774063
 HERE="$( cd "$(dirname "$0")" ; pwd -P )"
