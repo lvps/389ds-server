680f84156c841117147c9e9ed9c8200ea4f5de6f
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2019-04-14 22:17:28 +0200

    Script to generate certificates for different hosts

--- tests/cert.sh
+++ tests/cert.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
+
+HOSTNAME=${1:-ldaptest.example.local}
+ID=${HOSTNAME//\./_}
+export SAN=$HOSTNAME
+
 if [[ ! -f noise.bin ]]; then
 	 openssl rand -out noise.bin 4096
 fi
-if [[ -f test_cert.pem ]] && [[ -f test_key.pem ]]; then
+if [[ -f ${ID}_cert.pem ]] && [[ -f ${ID}.key ]]; then
 	echo "Reusing previous certificate"
 else
-	openssl req -x509 -newkey rsa:2048 -sha256 -keyout test_key.pem -out test_cert.pem -days 365 -nodes -extensions eku -config openssl.cnf -subj "/CN=ldaptest.local"
+	# https://security.stackexchange.com/a/86999
+	openssl req -x509 -newkey rsa:2048 -sha256 -keyout ${ID}.key -out ${ID}_cert.pem -days 365 -nodes -extensions eku -config openssl.cnf -subj "/CN=${HOSTNAME}"
 fi
 # https://stackoverflow.com/a/4774063
 HERE="$( cd "$(dirname "$0")" ; pwd -P )"
-echo Add these to the playbook:
-echo "tls_cert_file: \"$HERE/test_cert.pem\","
-echo "tls_key_file: \"$HERE/test_key.pem\","
+echo --- Add this to the playbook ---
+echo "tls_cert_file: \"$HERE/${ID}_cert.pem\""
+echo "tls_key_file: \"$HERE/${ID}.key\""
 echo "tls_files_remote: false"
+echo "tls_certificate_trusted: false"
+echo --------------------------------
